55492241643cd0b07cc0757195cb790b07d5c9ac
Rohith Jayawardene <gambol99@gmail.com>
2025-03-13 18:44:47 +0000

    chore: adding the argocd iam role used to assume permissions

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,8 @@ output "eks_cluster_name" {
   description = "The name of the EKS cluster"
   value       = module.eks.cluster_name
 }
+
+output "argocd_spoke_role_arn" {
+  description = "When provisioning a spoke, the is the IAM role the hub must assume"
+  value       = module.eks.cross_account_role_arn
+}
