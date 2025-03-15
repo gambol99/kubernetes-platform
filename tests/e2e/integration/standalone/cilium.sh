566d2fee0df202b1142dc51ce46658918223528d
Rohith Jayawardene <gambol99@gmail.com>
2025-03-15 14:19:43 +0000

    chore: adding unit tests to validate cilium is installed

---
+++ tests/e2e/integration/standalone/cilium.sh
@@ -0,0 +1,49 @@
+## Description: This script contains the common functions for the cilium tests
+
+load ../../lib/helper
+
+setup() {
+  [[ ! -f ${BATS_PARENT_TMPNAME}.skip ]] || skip "skip remaining tests"
+}
+
+teardown() {
+  [[ -n $BATS_TEST_COMPLETED   ]] || touch "${BATS_PARENT_TMPNAME}.skip"
+}
+
+@test "We should have an cilium application in argocd namespace" {
+  kubectl "get application -n argocd -l app.kubernetes.io/name=cilium | grep -q 'system-cilium'"
+}
+
+@test "We should have a cilium namespace" {
+  kubectl "get namespace cilium-system"
+}
+
+@test "We should have cilium custom resources configured" {
+  kubectl "get crd | grep -i cilium"
+}
+
+@test "We should have a cilium deployments" {
+  DEPLOYMENTS=(
+    cilium-operator
+    hubble-relay
+    hubble-ui
+  )
+
+  for DEPLOYMENT in "${DEPLOYMENTS[@]}"; do
+    kubectl "get deployment -n cilium-system ${DEPLOYMENT}"
+  done
+}
+
+@test "We should have running cilium pods" {
+  PODS=(
+    cilium-envoy
+    cilium-agent
+    cilium-operator
+    hubble-relay
+    hubble-ui
+  )
+
+  for POD in "${PODS[@]}"; do
+    kubectl "get pod -n cilium-system -l app.kubernetes.io/name=${POD} | grep -q 'Running'"
+  done
+}
